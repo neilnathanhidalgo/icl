1e56fae40fe27a197ebdee1f5ed70d7a3ff052f7
Nat Hidalgo <nathan_hidalgo@hotmail.com>
2023-04-04 11:21:44 -0500

    oauth corriendo

---
+++ oauth2/src/main/java/gob/pe/icl/oauth2/Oauth2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gob.pe.icl.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author NATHAN
+ */
+@SpringBootApplication
+public class Oauth2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2.class, args);
+    }
+}
